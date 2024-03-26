896fcbda82a46662b974fb4095838c140387bff1
Vishal <vishalwins08@gmail.com>
2024-03-26 10:29:33 +0530

    data

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-export const routes: Routes = [];
+
+
+export const routes: Routes = [
+    {
+        path: 'login',
+        loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+      }
+];
+
+// @NgModule({
+//   imports: [RouterModule.forRoot(routes)],
+//   exports: [RouterModule],
+// })
